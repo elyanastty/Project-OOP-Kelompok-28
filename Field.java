93eace5cf2bf760a7aa2de76e5c9dc8d66eb5de3
F1D022003_APRILIANINGSIH <NINGSIHAPRILIA195@GMAIL.COM>
2024-12-04 21:48:58 +0800

    Update Field.java

--- Field.java
+++ Field.java
@@ -3,11 +3,6 @@ package Field;
 import Main.Main;
 import java.util.Vector;
 
-/**
- * Abstract class Field
- * Kelas ini merupakan child dari Thread
- */
-
 public abstract class Field extends Thread {
     static protected boolean direction=true;
     protected final int fieldSize = 7;
@@ -15,21 +10,12 @@ public abstract class Field extends Thread {
     protected Vector<Boolean> item;
     private boolean run=true;
 
-    /**
-     * Konstruktor
-     */
     public Field(){
         rdirection=direction;
         direction = !direction;
         item = new Vector<>(fieldSize);
 	}
 
-    /**
-     * Method run
-     * Override method run dari thread
-     * Method ini menggerakan setiap class Water yang berada di 'Map'
-     * dengan setiap log di dalamnya
-     */
     public void run() {
         while (run && Main.player.getStatus()) {
             try {
@@ -59,42 +45,18 @@ public abstract class Field extends Thread {
         }
     }
 
-    /**
-     * Method stopThread
-     * mengubah boolean run menjadi false
-     */
     public void stopThread() {
         this.run=false;
     }
 
-    /**
-     * Method getDirection
-     * @return direction di Field ini
-     */
     public boolean getDirection(){return rdirection;}
 
-    /**
-     * Method setDirection
-     * @param _direction arah baru
-     */
     public void setDirection(boolean _direction){ rdirection = _direction; }
 
-    /**
-     * Method getFieldSize
-     * @return fieldSize
-     */
 	public int getFieldSize() { return fieldSize; }
 
-    /**
-     * Abstract method generateRandom
-     */
     public abstract void generateRandom();
 
-    /**
-     * Method moveField
-     * 'Memindahkan' Vector of Boolean dr indeks pertama ke indeks terakhir jika direction true
-     * atau sebaliknya, dari indeks terakhir ke indeks pertama jika direction false
-     */
     public void moveField(){
         if(rdirection) {
             boolean temp = item.remove(0);
@@ -105,20 +67,9 @@ public abstract class Field extends Thread {
         }
     }
 
-    /**
-     * Method isItem
-     * @param index indeks item
-     * @return true jika di index terdapat item
-     */
     public boolean isItem(int index){
         return item.elementAt(index);
     }
 
-    /**
-     * Method setItem
-     * @param index indeks yang akan diubah
-     * @param _item boolean baru
-     */
     public void setItem(int index, boolean _item) { item.setElementAt(_item, index);}
 }
-
